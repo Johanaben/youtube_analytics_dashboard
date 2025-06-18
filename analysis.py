741bee30c84c8b6acaad92f72c6870ac49702c6e
Johana Benedict <johanabenedict1126@gmail.com>
2025-06-18 21:51:03 +0400

    initial commit- ready to deploy

---
+++ analysis.py
@@ -0,0 +1,110 @@
+from googleapiclient.discovery import build
+import pandas as pd
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv("API_KEY")
+
+def get_channel(name):
+    url = 'https://www.googleapis.com/youtube/v3/search'
+    params = params = {
+    "part":"snippet",
+    "q": name,
+    "type":"channel",
+    "key": api_key
+    }
+    response = requests.get(url,params=params).json()
+
+    if not response.get('items'):
+        raise ValueError(f"No channel found for: {name}")
+    
+    try:
+        channel_id = response['items'][0]['id']['channelId']
+    except (IndexError, KeyError):
+        raise ValueError("Channel ID could not be extracted from API response.")
+    
+
+    ch_url = 'https://www.googleapis.com/youtube/v3/channels'
+    ch_params = {
+        "part": "statistics,snippet",
+        "id": channel_id,    
+        "key": api_key       
+    }
+    ch_response = requests.get(ch_url,params=ch_params).json()
+    return channel_id,ch_response
+
+def get_channel_datails(ch_response):
+    try:
+        data = ch_response['items'][0]
+        title = data['snippet']['title']
+        country = data['snippet'].get('country', 'Unknown')
+        tot_view = int(data['statistics']['viewCount'])
+        sub_count = int(data['statistics']['subscriberCount'])
+        vid_count = int(data['statistics']['videoCount'])
+
+        channel_stats = {
+            'title': title,
+            'country': country,
+            'views': tot_view,
+            'subscribers': sub_count,
+            'videos': vid_count
+        }
+        df = pd.DataFrame([channel_stats])
+        return df
+
+    except (IndexError, KeyError, ValueError) as e:
+        print("Error extracting channel details:", e)
+        return pd.DataFrame()
+
+def get_video_stats(channel_id):
+    search_url = 'https://www.googleapis.com/youtube/v3/search'
+    search_params = {
+        'part': 'snippet',
+        'channelId': channel_id,
+        'maxResults': 50,
+        'order': 'date',     # This is important to filter videos by date
+        'type': 'video',
+        'key': api_key
+    }
+
+    search_response = requests.get(search_url, params=search_params).json()
+    video_ids = [item['id']['videoId'] for item in search_response.get('items',[])]
+    
+    if not video_ids:
+        print("No videos found ")
+        return pd.DataFrame()
+
+    videos_url = 'https://www.googleapis.com/youtube/v3/videos'
+    videos_params = {
+        'part': 'snippet,statistics',
+        'id': ','.join(video_ids),
+        'key': api_key
+    }
+    videos_response = requests.get(videos_url, params=videos_params).json()
+
+    video_data = []
+
+    if 'items' not in videos_response:
+        print("No items in video stats response:", videos_response)
+        return pd.DataFrame()
+
+    for item in videos_response['items']:
+        video_data.append({
+            'title': item['snippet']['title'],
+            'upload_date': item['snippet']['publishedAt'],
+            'views': int(item['statistics'].get('viewCount', 0)),
+            'likes': int(item['statistics'].get('likeCount', 0))
+        })
+
+    df_videos = pd.DataFrame(video_data)
+
+    if not df_videos.empty:
+        df_videos['upload_date'] = pd.to_datetime(df_videos['upload_date'])
+    return df_videos
+
+
+
+
+
--- app.py
+++ app.py
@@ -1,12 +1,40 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
+from analysis import get_channel, get_channel_datails, get_video_stats
 
-st.set_page_config(page_title="Spotify Dashboard",layout="centered")
-st.header("Spotify API: Rhythm and Trends ")
-st.subheader("Aim of the project")
-st.write("It all started with me noticing something strange with my monthly spotify analytics."\
-         " For one particular month, it showed that my **slowest** and **fastest** song were the **exact same track** — lowest BPM and highest BPM at once." \
-"This weird glitch led down a rabbit hole." \
-"This project is a result of that curiosity fueled by my love for music theory and my growing interest in the world of data science.")
-st.markdown("<h3>What is the average BPM across genres?<br>Does it vary across genres?</h3>",unsafe_allow_html = True)
-st.subheader("Does BPM always mean fast paced?")
-st.subheader("The nuances: A little music theory lesson")
+st.set_page_config(page_title="YouTube Analytics",page_icon=':bar_chart:',layout="wide")
+st.title("YouTube Analytics Dashboard")
+st.markdown('<style>div.block-container{padding-top:2rem;}</style>',unsafe_allow_html=True)
+channel_name = st.text_input("Name of channel")
+if channel_name:
+    channel_id,channel_response = get_channel(channel_name)
+    channel_details = get_channel_datails(channel_response)
+    title = channel_details['title']
+    if hasattr(title, 'iloc'):
+        title = title.iloc[0]
+
+    subs = channel_details['subscribers']
+    if hasattr(subs, 'iloc'):
+        subs = subs.iloc[0]
+    st.subheader(f"Channel: {title}")
+    st.subheader(f"Subscriber Count: {subs}")
+    
+
+
+    video_details = get_video_stats(channel_id)
+
+    video_details.set_index("upload_date", inplace=True)
+
+    st.subheader("View Count Over Time")
+    st.line_chart(video_details['views'])
+
+
+    st.subheader("Views and Likes Over Time")
+    st.line_chart(video_details[['views', 'likes']])
+else:
+    st.warning("Please enter channel name.")
+
+
+
+
